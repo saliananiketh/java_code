ef3b89f82d360fee6689575935b541ff39f31849
saliananiketh <anikethhh69@gmail.com>
2025-01-29 09:41:14 +0530

    Update product.java

--- product.java
+++ product.java
@@ -4,7 +4,7 @@ class welcome
   {
     int a=5;
     int b=10;
-    product=a*b
-    System.out.println("Product of two integers is: ",product)
+    product=a*b;
+    System.out.println("Product of two integers is: "+product);
   }
 }
