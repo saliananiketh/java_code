4757592f091a6ec94e9de19d3866d5d1b8aba36c
saliananiketh <anikethhh69@gmail.com>
2025-01-29 09:37:23 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,10 @@
+class welcome
+{
+  public static void main(String args[])
+  {
+    int a=5;
+    int b=10;
+    product=a*b
+    System.out.println("Product of two integers is: ",product)
+  }
+}
